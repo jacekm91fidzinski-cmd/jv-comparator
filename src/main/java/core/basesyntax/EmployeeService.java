fabc58349b5df4d9e77ef4ca1601ce71c36beaf1
jacekm91fidzinski-cmd <jacekm91fidzinski@gmail.com>
2025-12-07 17:30:50 +0100

    jv-comparator

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,20 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+
+        Comparator<Employee> employeeComparator =
+                Comparator.comparing(Employee::getAge)
+                        .thenComparing(Employee::getName);
+
+        Set<Employee> sortedEmployees = new TreeSet<>(employeeComparator);
+        sortedEmployees.addAll(employees);
+
+        return sortedEmployees;
     }
 }
